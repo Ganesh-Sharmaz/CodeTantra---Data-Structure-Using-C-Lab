2e8c69a49dd1a558f89d86ec4714dfa3792441f7
Ganesh Sharma <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 16:03:55 +0530

    Create Q1.c

---
+++ Lab Program 1/Q1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main(){
+    int a[26],i,n,], temp;
+    printf("n : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf(*alxd] = ",1);
+        scanf("%d",2a[i]);
+    }
+
+
+    printf("Before sorting : \n");
+    for(i=0;i<n;i++){
+        printf("a[%d] = %d\n",1,a[i]);
+    }
+    for(i=n-2;1>=0;1--){
+        for(j=0;3<=i;3++){
+            if(al3l>ali+1{
+
+                temp=a[j];
+
+                alil-al3+1];
+
+                a[j+1]-temp;
+            }
+        }
+    }
+
+
+    printf("After sorting : \n");
+    for(i=0;i<n;i++){
+        printf("a[%d] = %d\n",1,a[i]);
+
+    }
+}
